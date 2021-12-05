5a99be7e16479d690a7aad7d1aca0c6e89b89838
chars <yaojiupeng4558@gmail.com>
2021-12-06 01:36:31 +0800

    build

--- 3dtest/build/build.sh
+++ 3dtest/build/build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-Echo "Hello World"
+project_path="../3dtest"
+
+output_path="/Users/chars"
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -projectPath ${project_path} -executeMethod ReleaseTool.BuildAndroid -quit
+
+cd ${project_path}
+
+cp k2.pkg ${output_path}
+
+rm -rf k2.pkg
